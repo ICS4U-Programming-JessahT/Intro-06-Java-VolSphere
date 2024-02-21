784e081943e0349166fc9e0a35c911ad568e6817
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2024-02-21 15:42:02 +0000

    Addedv1 VolSphere.java

--- VolSphere.java
+++ VolSphere.java
@@ -7,33 +7,27 @@ import java.util.Scanner;
  * @version 1.0
  * @since 2024-02-15
  */
-public final class VolSphere {
-
-  private VolSphere() {
-    // whitespace
-  }
-
-  /**
-   * Print message.
-   *
-   * @param args Command-line arguments (not used in program).
-   */
+final public class VolSphere {
 
   // Declare variables
   static float UserNum;
 
-  public static void main(final String[] args) {
+  public static void main(final String[] args) { // Calculator
+
     // Get user input
-    Scanner input = new Scanner(System.in);
-    System.out.println("Sphere Volume Calculator, Enter a radius:");
-    UserNum = input.nextFloat();
+    System.out.print("Sphere Volume Calculator, Enter a radius: ");
+    final Scanner input = new Scanner(System.in);
+    final float UserNum = input.nextFloat();
+
 
     // use if else statements to check if user can vote
     if (UserNum >= 1) {
       // Output for right value.
-      final double Volume = (4d / 3d) * (Math.PI) * (Math.pow(UserNum, 3));
-      System.out.println("The volume of a circle with the radius of");
-      System.out.format(UserNum + " is " + "%.3f", Volume + "cm");
+      final double SphVol = (4d / 3d) * (Math.PI) * (Math.pow(UserNum, 3));
+      System.out.print("The volume of a circle with the radius of ");
+      System.out.print(UserNum + " is ");
+      System.out.format("%.3f", SphVol);
+      System.out.println();
 
     } else {
       // Output for incorrect value.
